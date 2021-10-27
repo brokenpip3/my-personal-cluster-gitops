be032d91b0919b886590b0f2235653441424f287
brokenpip3 <brokenpip3@gmail.com>
2021-10-28 00:25:53 +0200

    jenkins: universal kaniko build check if tag exist

--- ci-cd/jenkins-general-jobs-collection/kaniko-universal-docker-build.groovy
+++ ci-cd/jenkins-general-jobs-collection/kaniko-universal-docker-build.groovy
@@ -1,7 +1,8 @@
 pipeline {
 		agent {
 				kubernetes {
-						yaml '''
+                    defaultContainer 'kaniko'
+                    yaml '''
 kind: Pod
 apiVersion: v1
 metadata:
@@ -20,6 +21,13 @@ spec:
     volumeMounts:
     - name: jenkins-docker-cfg
       mountPath: /kaniko/.docker
+  - name: utils
+    workingDir: /tmp/jenkins
+    image: brokenpip3/ubuntu-curl-jq
+    command:
+    - sleep
+    args:
+    - infinity
   volumes:
   - name: jenkins-docker-cfg
     secret:
@@ -51,32 +59,55 @@ spec:
 				string(name: 'IMAGE_DOCKERFILE_DIR', defaultValue: '.', description: 'Image dockerfile path')
 		}
 
+        //curl -s https://quay.io/api/v1/repository/brokenpip3/tg2| jq -e -r '.tags[] | select(.name=="0.0.2")| any'  > /dev/null
 		stages {
+				stage('Check if tag exist') {
+                    when {  expression { params.GIT_BRANCH.isEmpty() }}
+                    steps {
+                            script {
+                                container(name: 'utils') {
+                                    final String url = "https://quay.io/api/v1/repository/${params.IMAGE_ORG}/${params.IMAGE_NAME}"
+                                    final def imagereturncode = sh(script: """
+                                    #!/usr/bin/env bash
+                                    curl -s $url | jq -r '.tags[] | select(.name==\"$GIT_TAG\") | any'
+                                    """, returnStdout: true).trim()
+
+                                    if (imagereturncode == "true") {
+                                        env.IMAGE_EXIST = "true"
+                                    } else {
+                                        env.IMAGE_EXIST = "false"
+                                    }
+                                }
+                            }
+                        }
+                    }
 				stage('Checkout') {
-						steps {
-						        // If git tag is specified checkout tag and the same docker img tag
-								// otherwise use branch and "latest" as docker img tag
-								script {
-										if (params.GIT_TAG.isEmpty()) {
-												checkout([
-												$class: 'GitSCM',
-												branches: [[name: "refs/heads/${GIT_BRANCH}"]],
-												userRemoteConfigs: [[ url: "${GIT_URL}"]]
-												])
-												env.IMAGE_TAG = "latest"
-										} else {
-												checkout([
-												$class: 'GitSCM',
-												branches: [[name: "refs/heads/${GIT_TAG}"]],
-												userRemoteConfigs: [[ url: "${GIT_URL}"]]
-												])
-										}
-								}
+                    when { expression { return env.IMAGE_EXIST == 'false'} }
+					steps {
+						    // If git tag is specified checkout tag and the same docker img tag
+							// otherwise use branch and "latest" as docker img tag
+							script {
+                                if (params.GIT_TAG.isEmpty()) {
+                                    checkout([
+                                    $class: 'GitSCM',
+                                    branches: [[name: "refs/heads/${GIT_BRANCH}"]],
+                                    userRemoteConfigs: [[ url: "${GIT_URL}"]]
+                                    ])
+                                    env.IMAGE_TAG = "latest"
+                                } else {
+									checkout([
+									$class: 'GitSCM',
+									branches: [[name: "refs/heads/${GIT_TAG}"]],
+									userRemoteConfigs: [[ url: "${GIT_URL}"]]
+                                    ])
+                                }
+                            }
 						}
 				}
 				stage('Make Image') {
-						steps {
-								container(name: 'kaniko', shell: '/busybox/sh') {
+                    when { expression { return env.IMAGE_EXIST == 'false'} }
+                    steps {
+                            container(name: 'kaniko', shell: '/busybox/sh') {
 										sh """#!/busybox/sh
 										   /kaniko/executor -f ${IMAGE_DOCKERFILE_PATH} -c ${IMAGE_DOCKERFILE_DIR} --cache=false \
 										     --destination=${IMAGE_REGISTRY}/${IMAGE_ORG}/${IMAGE_NAME}:${IMAGE_TAG}
