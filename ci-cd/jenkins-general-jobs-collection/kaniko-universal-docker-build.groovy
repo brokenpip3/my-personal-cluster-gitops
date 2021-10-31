7727a41d63d722ed9611ed51476d67bdce46664d
brokenpip3 <brokenpip3@gmail.com>
2021-10-31 15:58:55 +0100

    jenkins: kaniko build add resources and comments

--- ci-cd/jenkins-general-jobs-collection/kaniko-universal-docker-build.groovy
+++ ci-cd/jenkins-general-jobs-collection/kaniko-universal-docker-build.groovy
@@ -1,6 +1,19 @@
 // Universal docker image build ci with Kanikp
 //
-// TDB
+// Condition for image build:
+// - If GIT_BRANCH is specified build image from that branch with IMAGE_TAG equals to latest
+// - If GIT_BRANCH is specified and GIT_FORCE_LAST_TAG is enabled build image from last tag on that branch if does not alredy exist on quay.io
+// - if GIT_TAG is specified build image from that tag if it does not already exist on quay.io
+//
+// Other assumptions:
+// - If IMAGE_NAME is empty repo name is used
+// - If IMAGE_TAG is empyty and GIT_TAG is provided set image tag equals to git tag
+//
+// Please be aware that due to:
+// - https://github.com/jenkinsci/kubernetes-plugin#notedue-to-implementation-constraints-there-can-be-issues-when-executing-commands-in-different-containers-if-they-run-using-different-uidsit-is-recommended-to-use-the-same-uid-across-the-different-containers-part-of-the-same-pod-to-avoid-any-issue
+// - https://github.com/GoogleContainerTools/kaniko/issues/105
+// the containers must run with the same user id
+// and since kaniko need id 0 the containers will run as root user
 
 pipeline {
 		agent {
@@ -25,13 +38,30 @@ spec:
     volumeMounts:
     - name: jenkins-docker-cfg
       mountPath: /kaniko/.docker
+    resources:
+      limits:
+        memory: 2Gi
+        cpu: 1500m
+        ephemeral-storage: 5Gi
+      requests:
+        memory: 1Gi
+        cpu: 500m
+        ephemeral-storage: 5Gi
   - name: utils
     workingDir: /tmp/jenkins
-    image: brokenpip3/ubuntu-toolbox
+    image: brokenpip3/ubuntu-toolbox-jenkins
+    tty: true
     command:
     - sleep
     args:
     - infinity
+    resources:
+      limits:
+        memory: 500Mi
+        cpu: 500m
+      requests:
+        memory: 500Mi
+        cpu: 500m
   volumes:
   - name: jenkins-docker-cfg
     secret:
@@ -47,8 +77,9 @@ spec:
 				container = 'docker'
 		}
 
-		options { skipDefaultCheckout true
-		timeout(time: 15, unit: 'MINUTES')
+		options {
+            skipDefaultCheckout true
+            timeout(time: 15, unit: 'MINUTES')
 		}
 
 		parameters {
@@ -65,39 +96,65 @@ spec:
 		}
 
 		stages {
-                stage('validation') {
+				stage('Checkout') {
+                    steps {
+                        // If git tag is specified checkout tag and set docker img tag otherwise use branch
+                        script {
+                            if (params.GIT_TAG.isEmpty()) {
+                                checkout([
+                                    $class: 'GitSCM',
+                                    branches: [[name: "refs/heads/${params.GIT_BRANCH}"]],
+                                    userRemoteConfigs: [[ url: "${params.GIT_URL}"]]
+                                    ])
+                            } else {
+								checkout([
+									$class: 'GitSCM',
+									branches: [[name: "refs/tags/${params.GIT_TAG}"]],
+									userRemoteConfigs: [[ url: "${params.GIT_URL}"]]
+                                    ])
+                                }
+                            }
+						}
+                }
+                stage('Validation') {
                     steps {
                         // Check if some optional params are empty and fill them with git info
                         script {
-                            // If image name is not procided set it from repo name
-                            // Example https://github.com/foo/bar will set image name = bar
-                            if (params.IMAGE_NAME.isEmpty()) {
-                                env.IMAGE_NAME = params.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
-                            }
-                            // If image tag is empty but git tag is provided
-                            // set image tag equals to git tag
-                            if (params.IMAGE_TAG.isEmpty() && env.GIT_BRANCH.isEmpty()) {
-                                env.IMAGE_TAG = params.GIT_TAG
+                            container(name: 'utils') {
+                                // If image name is not provided set it from repo name
+                                // Example https://github.com/foo/bar will set image name = bar
+                                if (params.IMAGE_NAME.isEmpty()) {
+                                    env.IMAGE_NAME = params.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
+                                }
+                                // If image tag is empty but git tag is provided
+                                // set image tag equals to git tag
+                                if (params.IMAGE_TAG.isEmpty() && env.GIT_BRANCH.isEmpty()) {
+                                    env.IMAGE_TAG = params.GIT_TAG
+                                }
+                                // if GIT_FORCE_LAST_TAG is enabled set last git tag as image tag
+                                // otherwise use latest
+                                if (params.GIT_TAG.isEmpty()) {
+                                    if (params.GIT_FORCE_LAST_TAG) {
+                                        final def lasttag = sh(script: "git describe --tags --abbrev=0", returnStdout: true).trim()
+                                        env.IMAGE_TAG = lasttag
+                                    } else {
+                                        env.IMAGE_TAG = "latest"
+                                    }
+                                }
                             }
                         }
                     }
                 }
 				stage('Check if tag exist') {
-                    when {
-                        allOf {
-                            expression { return params.GIT_BRANCH.isEmpty() }
-                            not {
-                                expression { return params.GIT_TAG.isEmpty() }
-                            }
-                        }
-                    }
+                    // Check if the tag exist in quay.io
+                    when { expression { env.IMAGE_TAG != "latest" } }
                     steps {
                             script {
                                 container(name: 'utils') {
-                                    final String url = "https://quay.io/api/v1/repository/${params.IMAGE_ORG}/${params.IMAGE_NAME}"
+                                    final String url = "https://quay.io/api/v1/repository/${params.IMAGE_ORG}/${env.IMAGE_NAME}"
                                     final def imagereturncode = sh(script: """
                                     #!/usr/bin/env bash
-                                    curl -s $url | jq -r '.tags[] | select(.name==\"$GIT_TAG\") | any'
+                                    curl -s $url | jq -r '.tags[] | select(.name==\"$IMAGE_TAG\") | any'
                                     """, returnStdout: true).trim()
 
                                     if (imagereturncode == "true") {
@@ -109,48 +166,12 @@ spec:
                             }
                         }
                     }
-				stage('Checkout') {
-                    when {
-                        anyOf {
-                            expression { return env.IMAGE_EXIST == 'false'}
-                            not {
-                                expression { return params.GIT_BRANCH.isEmpty()}
-                            }
-                        }
-                    }
-                    steps {
-						    // If git tag is specified checkout tag and set docker img tag
-							// otherwise use branch and set "latest" as docker img tag
-							script {
-                                if (params.GIT_TAG.isEmpty()) {
-                                    checkout([
-                                    $class: 'GitSCM',
-                                    branches: [[name: "refs/heads/${params.GIT_BRANCH}"]],
-                                    userRemoteConfigs: [[ url: "${params.GIT_URL}"]]
-                                    ])
-                                    if (params.GIT_FORCE_LAST_TAG) {
-                                        final def LASTTAG = sh(returnStdout: true, script: "git describe --tags --abbrev=0").trim()
-                                        env.IMAGE_TAG = LASTTAG
-                                    } else {
-                                        env.IMAGE_TAG = "latest"
-                                    }
-                                } else {
-									checkout([
-									$class: 'GitSCM',
-									branches: [[name: "refs/tags/${params.GIT_TAG}"]],
-									userRemoteConfigs: [[ url: "${params.GIT_URL}"]]
-                                    ])
-                                }
-                            }
-						}
-				}
 				stage('Make Image') {
+                    // Build image with Kaniko only if does not exist on destination or is latest
                     when {
                         anyOf {
                             expression { return env.IMAGE_EXIST == 'false'}
-                            not {
-                                expression { return params.GIT_BRANCH.isEmpty()}
-                            }
+                            expression { return env.IMAGE_TAG == "latest" }
                         }
                     }
                     steps {
