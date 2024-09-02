b3610094c37ddbd511307458c8c395750a4365f6
brokenpip3 <brokenpip3@gmail.com>
2024-09-02 09:18:52 +0200

    nixify validate flow

--- flake.nix
+++ flake.nix
@@ -10,24 +10,24 @@
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        validationpkgs = [ pkgs.kubeconform pkgs.kustomize pkgs.yq-go ];
       in
       {
-        # nix fmt formatter
         formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
 
-        # default devshell
         devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
-          packages = [
-            pkgs.pre-commit
-            pkgs.go-task
-            pkgs.fluxcd
-            pkgs.kubeconform
-            pkgs.kustomize
-            pkgs.yq-go
+          packages = with pkgs; [
+            pre-commit
+            go-task
+            fluxcd
+            validationpkgs
+            (writeShellApplication {
+              name = "validate";
+              runtimeInputs = validationpkgs;
+              text = builtins.readFile ./scripts/github/validate.sh;
+            })
           ];
 
-          # Will be executed before entering the shell
-          # or running a command
           shellHook = ''
           '';
         };
--- scripts/github/validate.sh
+++ scripts/github/validate.sh
@@ -3,31 +3,9 @@
 ###
 # FROM https://github.com/fluxcd/flux2-kustomize-helm-example/tree/main/scripts
 ###
-# TODO: nixify this script
-
-# This script downloads the Flux OpenAPI schemas, then it validates the
-# Flux custom resources and the kustomize overlays using kubeconform.
-# This script is meant to be run locally and in CI before the changes
-# are merged on the main branch that's synced by Flux.
-
-# Copyright 2023 The Flux authors. All rights reserved.
+# Changes
+# - Added 'yq "del(.sops)"' to remove the sops struct from the schema so we can validate secret as well
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Prerequisites
-# - yq v4.34
-# - kustomize v5.3
-# - kubeconform v0.6
 
 set -o errexit
 set -o pipefail
@@ -37,7 +15,7 @@ kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
 
 # skip Kubernetes Secrets due to SOPS fields failing validation
-kubeconform_flags=("-skip=Secret" "-ignore-filename-pattern=.sops.yaml")
+kubeconform_flags=("-ignore-filename-pattern=.sops.yaml")
 kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
 echo "INFO - Downloading Flux OpenAPI schemas"
@@ -61,12 +39,12 @@ done
 
 echo "INFO - Validating kustomize overlays"
 echo "Temporary disabled"
-#find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-#  do
-#    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-#    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
-#      kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
-#    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-#      exit 1
-#    fi
-#done
+find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+  do
+    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | yq "del(.sops)" | \
+      kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+      exit 1
+    fi
+done
